1d15800d10d66f6ff71d0bda532e4432438276c1
sviet_ka <sviet_ka@mail.ru>
2023-10-26 23:44:52 -0700

    init: app boilerplate code added

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Component, ReactNode } from 'react';
+import './App.css';
+
+class App extends Component {
+  render(): ReactNode {
+    return <div> Hello world! </div>;
+  }
+}
+
+export default App;
