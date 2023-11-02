9f156141c807da8a5ef0e72a4adc9713980a83fb
sviet_ka <sviet_ka@mail.ru>
2023-11-02 00:36:43 -0700

    refactor: create MainPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,10 @@
-import React, { useCallback, useState } from 'react';
 import './App.css';
-import SearchBar from './components/search_bar/SearchBar';
-import { Movie, searchMovies } from './services/MovieService';
-import SearchResults from './components/search_results/SearchResults';
+import MainPage from './components/pages/main_page/MainPage';
 
 const App: React.FC = () => {
-  const [results, setResults] = useState<Movie[]>([]);
-  const [isLoaded, setisLoaded] = useState<boolean>(true);
-  const [isThrowButtonClicked, setisThrowButtonClicked] =
-    useState<boolean>(false);
-
-  const handleSearch = useCallback(
-    async (searchString: string): Promise<void> => {
-      setisLoaded(false);
-      const requestResult = await searchMovies(searchString);
-      setResults(requestResult);
-      setisLoaded(true);
-    },
-    []
-  );
-  const handleThrowErrow = (): void => {
-    setisThrowButtonClicked(true);
-  };
-
-  if (isThrowButtonClicked) {
-    throw new Error('I crashed!');
-  }
   return (
     <div>
-      <SearchBar onSearch={handleSearch}></SearchBar>
-      <div className="section">
-        <button className="throw-error-button" onClick={handleThrowErrow}>
-          Throw Error
-        </button>
-      </div>
-      {isLoaded ? (
-        <SearchResults results={results}></SearchResults>
-      ) : (
-        <div className="section">Loading...</div>
-      )}
+      <MainPage />
     </div>
   );
 };
---
+++ src/components/pages/main_page/MainPage.tsx
@@ -0,0 +1,46 @@
+import React, { useCallback, useState } from 'react';
+import './MainPage.css';
+import SearchBar from '../../../components/search_bar/SearchBar';
+import { Movie, searchMovies } from '../../..//services/MovieService';
+import SearchResults from '../../../components/search_results/SearchResults';
+
+const MainPage: React.FC = () => {
+  const [results, setResults] = useState<Movie[]>([]);
+  const [isLoaded, setisLoaded] = useState<boolean>(true);
+  const [isThrowButtonClicked, setisThrowButtonClicked] =
+    useState<boolean>(false);
+
+  const handleSearch = useCallback(
+    async (searchString: string): Promise<void> => {
+      setisLoaded(false);
+      const requestResult = await searchMovies(searchString);
+      setResults(requestResult);
+      setisLoaded(true);
+    },
+    []
+  );
+  const handleThrowErrow = (): void => {
+    setisThrowButtonClicked(true);
+  };
+
+  if (isThrowButtonClicked) {
+    throw new Error('I crashed!');
+  }
+  return (
+    <div>
+      <SearchBar onSearch={handleSearch}></SearchBar>
+      <div className="section">
+        <button className="throw-error-button" onClick={handleThrowErrow}>
+          Throw Error
+        </button>
+      </div>
+      {isLoaded ? (
+        <SearchResults results={results}></SearchResults>
+      ) : (
+        <div className="section">Loading...</div>
+      )}
+    </div>
+  );
+};
+
+export default MainPage;
