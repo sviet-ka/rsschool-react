7f63f629301b079771808f124d7596c228a8e384
sviet_ka <sviet_ka@mail.ru>
2023-11-04 00:19:54 -0700

    feat: add base routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import ErrorBoundary from './components/error_boundary/ErrorBoundry';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './components/pages/not_found_page/NotFoundPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<p>Something went wrong</p>}>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </React.StrictMode>
 );
