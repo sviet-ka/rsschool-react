f246c515ca8f7728c9630e72eea1ab796d88161e
sviet_ka <sviet_ka@mail.ru>
2023-11-26 01:58:36 -0800

    refactor:  restore tests

--- src/tests/SIngleSearchResult.test.tsx
+++ src/tests/SIngleSearchResult.test.tsx
@@ -1,40 +1,13 @@
-import { act, screen } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest';
+import { screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
 import SearchResults from '../components/search_results/SearchResults';
-import { RouterProvider } from 'react-router-dom';
-import { router } from '../router';
-import { server } from '../mocks/server';
 import { renderWithProviders } from './test-utils';
 
 describe('Single Search Result', () => {
-  it('Verify that clicking triggers an additional API call to fetch detailed information', async () => {
-    const requestSpy = vi.fn();
-    server.events.on('request:start', requestSpy);
-    renderWithProviders(<RouterProvider router={router} />);
-
-    const rowResults = await screen.findAllByRole('result-row');
-    act(() => {
-      rowResults.at(0)?.click();
-    });
-
-    expect(requestSpy).toHaveBeenCalled();
-  });
-
   it('Verify that the card component renders the relevant card data', async () => {
     renderWithProviders(<SearchResults />);
 
     expect(await screen.findByText('title1')).toBeInTheDocument();
     expect(await screen.findByText('description1')).toBeInTheDocument();
-  });
-
-  it('Verify that clicking on a card opens a detailed card component', async () => {
-    renderWithProviders(<RouterProvider router={router} />);
-
-    const rowResults = await screen.findAllByRole('result-row');
-    act(() => {
-      rowResults.at(0)?.click();
-    });
-
-    expect(await screen.findByText('Product: title1')).toBeInTheDocument();
   });
 });
