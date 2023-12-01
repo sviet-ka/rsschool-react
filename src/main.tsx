e9ee0b3b58aee23733efc8c611bd5e35467d5ec6
sviet_ka <sviet_ka@mail.ru>
2023-11-30 23:58:49 -0800

    feat: add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'uncontrolled-form',
+        element: <h1>UncontrolledForm</h1>,
+      },
+      {
+        path: 'controlled-form',
+        element: <h1>ControlledForm</h1>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
